a9583e59eeef7ad130634bb6fdfefee52e4b400c
suzppk <ifc@suz-ppk.ru>
2023-09-15 14:37:37 +1000

    +1 метод

---
+++ Polyclinic Schedule/Controllers/UserCabinetController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Polyclinic_Schedule.DB;
+
+namespace Polyclinic_Schedule.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserCabinetController : ControllerBase
+    {
+        readonly User30Context db;
+
+        public UserCabinetController(User30Context db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            try
+            {
+                var user = await db.Users.
+                    Include(s => s.Schedules).
+                    FirstOrDefaultAsync(s => s.Id == id);
+                if (user == null)
+                    return NotFound();
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($" {ex.Message}");
+            }
+        }
+    }
+}
