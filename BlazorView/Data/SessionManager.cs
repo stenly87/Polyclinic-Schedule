da49fa49cf9604e233de77bd0e21d10a48aa55af
suzppk <ifc@suz-ppk.ru>
2023-09-20 16:20:04 +1000

    TODO

---
+++ BlazorView/Data/SessionManager.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorView.Data
+{
+    public class SessionManager
+    {
+        Dictionary<string, int> userSesssion = new();
+
+        public string GenerateSession(int idUser)
+        {
+            string session = Guid.NewGuid().ToString();
+            userSesssion.Add(session, idUser);
+            return session;
+        }
+    }
+}
